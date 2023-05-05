22647d3af9e16db9d95c8bc212a2f7f41faf2457
Nooblyl <1004969714@qq.com>
2023-05-05 16:30:18 +0800

    1

---
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -0,0 +1,89 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Dept;
+import com.itheima.pojo.Result;
+import com.itheima.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 部门管理Controller
+ */
+@Slf4j
+@RequestMapping("/depts")
+@RestController
+public class DeptController {
+
+    //private static Logger log = LoggerFactory.getLogger(DeptController.class);
+    @Autowired
+    private DeptService deptService;
+
+    /**
+     * 查询部门数据
+     * @return
+     */
+    //@RequestMapping(value = "/depts",method = RequestMethod.GET) //指定请求方式为GET
+    @GetMapping
+    public Result list(){
+        log.info("查询全部部门数据");
+        //调用service查询部门数据
+        List<Dept> deptList =  deptService.list();
+        return Result.success(deptList);
+    }
+
+
+    /**
+     * 删除部门
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id){
+        log.info("根据id删除部门:{}",id);
+        //调用service删除部门
+        deptService.delete(id);
+        return Result.success();
+    }
+
+
+    /**
+     * 新增部门
+     * @return
+     */
+    @PostMapping
+    public Result add(@RequestBody Dept dept){
+        log.info("新增部门: {}" , dept);
+        //调用service新增部门
+        deptService.add(dept);
+        return Result.success();
+    }
+
+    /**
+     * 查询部门
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Result search(@PathVariable Integer id){
+        log.info("根据id查询部门:{}",id);
+        //调用service查询部门
+        Dept dept = deptService.search(id);
+        return Result.success(dept);
+    }
+
+
+    /**
+     * 修改部门
+     * @return
+     */
+    @PutMapping
+    public Result update(@RequestBody Dept dept){
+        log.info("修改部门: {}", dept);
+        //调用service修改部门
+        deptService.update(dept);
+        return Result.success();
+    }
+}
---
+++ src/main/java/com/itheima/service/DeptService.java
@@ -0,0 +1,40 @@
+package com.itheima.service;
+
+import com.itheima.pojo.Dept;
+
+import java.util.List;
+
+/**
+ * 部门管理
+ */
+public interface DeptService {
+    /**
+     * 查询全部部门数据
+     * @return
+     */
+    List<Dept> list();
+
+    /**
+     * 删除部门
+     * @param id
+     */
+    void delete(Integer id);
+
+    /**
+     * 新增部门
+     * @param dept
+     */
+    void add(Dept dept);
+
+    /**
+     * 修改部门
+     * @param dept
+     */
+    void update(Dept dept);
+
+    /**
+     * 查询部门
+     * @param dept
+     */
+    Dept search(Integer id);
+}
